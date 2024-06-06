fa68a0f9ce25634a96ffe6ab4d6d5fdba4d11641
Bismar-nV <nogalesvallejosbismar136@gmail.com>
2024-06-05 20:16:41 -0400

    terminado

---
+++ repo/pdi_Imagen/PRIMER MOMENTO EVALUATIVO/practica.1/practic1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero para la serie fibonacci");
+            int x;
+            if (!int.TryParse(Console.ReadLine(), out x) || x <= 0)
+            {
+                Console.WriteLine("Entrada inválida. Por favor, ingrese un número valido");
+                return;
+            }
+
+            Console.WriteLine("Serie de Fibonacci hasta el límite especificado");
+            Sfibonacci(x);
+            return;
+        }
+        static void Sfibonacci(int x)
+        {
+            int numAnterior = 0;
+            int numActual = 1;
+
+            while (numActual <= x)
+            {
+                Console.WriteLine(numActual);
+                int siguiente = numAnterior + numActual;
+                numAnterior = numActual;
+                numActual = siguiente;
+            }
+        }
+    }
+}
